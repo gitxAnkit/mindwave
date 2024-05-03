0eea604ca3618412a7bfdb57a7190b9e4c0b7124
Ankit <132276650+gitxAnkit@users.noreply.github.com>
2024-05-03 19:41:02 +0530

    Hero sectoin

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import ButtonGradient from './assets/svg/ButtonGradient'
-import Button from './components/Button'
-import Header from './components/Header'
-import { BrowserRouter as Router } from 'react-router-dom'
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
+import Hero from "./components/Hero";
 const App = () => {
-
   return (
     <Router>
-      <div className='pt-[4.75re] lg:pt-[5.25rem] overflow-hidden '>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden ">
         <Header />
-        {/* <Button className='mt-10'  >Something</Button> */}
+        <Hero />
       </div>
 
       <ButtonGradient />
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
